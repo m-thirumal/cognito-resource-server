9584f60353924c7c4e518cc988c097159020749f
Thirumal <m.thirumal@hotmail.com>
2022-08-09 20:04:00 +0530

    first commit

---
+++ src/main/java/in/thirumal/CognitoResourceServerApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoResourceServerApplication.class, args);
+	}
+
+}
